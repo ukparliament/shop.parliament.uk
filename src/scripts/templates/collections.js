e797a0fd73bf86c4a281bc9f0e290f2f67f0a74f
Jake Mulley <mulleyjb@parliament.uk>
2017-05-11 11:30:36 +0100

    feature(collection-sorting): add JS to make it work

---
+++ src/scripts/templates/collections.js
@@ -0,0 +1,20 @@
+function replaceUrlParam(e,r,a){var n=new RegExp("("+r+"=).*?(&|$)"),c=e;return c=e.search(n)>=0?e.replace(n,"$1"+a+"$2"):c+(c.indexOf("?")>0?"&":"?")+r+"="+a};
+
+theme.collectionViews = function () {
+  var $changeView = $('[data-view]');
+  if ($changeView.length) {
+    $changeView.on('click', function() {
+      var view = $(this).data('view'),
+          url = document.URL,
+          hasParams = url.indexOf('?') > -1;
+
+      if (hasParams) {
+        window.location = replaceUrlParam(url, 'view', view);
+      } else {
+        window.location = url + '?view=' + view;
+      }
+    });
+  }
+};
+
+theme.collectionViews();
