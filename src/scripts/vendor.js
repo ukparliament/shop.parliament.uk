1c451e89b62b8b87d798f95f56fc379861d24742
Jake Mulley <mulleyjb@parliament.uk>
2017-06-29 15:06:53 +0100

    feature(image-zoom): add PoC

--- src/scripts/vendor.js
+++ src/scripts/vendor.js
@@ -8,6 +8,11 @@
  */
 // =require vendor/jquery-2.2.3.min.js
 
+/*!
+ * Magnifier.js
+ */
+// =require vendor/magnifier.js
+
 // Attempts to preserve comments that likely contain licensing information,
 // even if the comment does not have directives such as `@license` or `/*!`.
 //
